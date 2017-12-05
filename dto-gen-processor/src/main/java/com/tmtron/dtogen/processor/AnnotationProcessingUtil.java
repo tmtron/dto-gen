bcee1d7ff35a1b40dd2feea93d5889194a0cf215
TMTron <martin.trummer@tmtron.com>
2017-12-05 15:49:08 +0100

    removed unused code

--- dto-gen-processor/src/main/java/com/tmtron/dtogen/processor/AnnotationProcessingUtil.java
+++ dto-gen-processor/src/main/java/com/tmtron/dtogen/processor/AnnotationProcessingUtil.java
@@ -16,13 +16,7 @@
 package com.tmtron.dtogen.processor;
 
 import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.Map;
 import java.util.function.Supplier;
-
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
-import javax.lang.model.element.ExecutableElement;
 
 class AnnotationProcessingUtil {
 
@@ -37,52 +31,4 @@ class AnnotationProcessingUtil {
         return zonedDateTimeProvider.get();
     }
 
-    /**
-     * Returns the AnnotationValue with the elementNameSimple from the annotationMirror
-     * or throws a {@link RuntimeException} when the element does not exist
-     *
-     * @param annotationMirror  must contain the element
-     * @param elementNameSimple the simple name of the element
-     */
-    static AnnotationValue getRequiredAnnotationValue(AnnotationMirror annotationMirror,
-                                                      String elementNameSimple) {
-        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry :
-                annotationMirror.getElementValues().entrySet()) {
-            if (elementNameSimple.equals(entry.getKey().getSimpleName().toString())) {
-                // e.g. "value" -> {com.test.Dummy.ColorEnum.class, com.test.Dummy.BoolEnum.class}
-                return entry.getValue();
-            }
-        }
-        throw new RuntimeException("Required annotation value '" + elementNameSimple + "' not found");
-    }
-
-    /**
-     * Will do the type checking to assure that the given object is indeed a list
-     * which only contains items of type itemsClass and returns the object cast to
-     * the list-type.
-     *
-     * @param object     the object which must be of type List of T
-     * @param itemsClass the required type for all items in the object list
-     * @param objName    the name of the object (will be used for thrown Exceptions)
-     * @param <T>        the type of the list-elements (itemClass)
-     * @return the object cast to a list of T
-     * @throws RuntimeException when object is not a list of T
-     */
-    @SuppressWarnings("unchecked")
-    static <T> List<T> asList(Object object, Class<T> itemsClass, String objName) {
-        if (!(object instanceof List<?>)) {
-            throw new RuntimeException("The annotation member '" + objName + "' must be an array/list.");
-        }
-        List<?> objList = (List<?>) object;
-        objList.forEach(
-                item -> {
-                    if (!(itemsClass.isAssignableFrom(item.getClass()))) {
-                        throw new RuntimeException("The annotation member '" + objName + "' (array/list)"
-                                + " must only contain items of type: " + itemsClass.getCanonicalName());
-                    }
-                }
-        );
-        // the loop above has check that all items are okay
-        return (List<T>) objList;
-    }
 }
